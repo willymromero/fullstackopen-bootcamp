4af384599a0d07a6d4fbcb4b7122fe84149d9ea5
wrom <whunter0111@gmail.com>
2023-03-11 21:34:09 -0500

    "Part 1, exercise # 1.3 rocks:rocket:"
    - using sample array of three parts

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,63 +1,55 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-const Header = (props) => {
+const Header = ({ course }) => <h1>{course}</h1>;
+
+const Part = ({ name, exercises }) => (
+  <p>
+    {name} {exercises}
+  </p>
+);
+
+const Content = ({ parts }) => {
+  console.log(parts);
   return (
     <>
-      <h1>{props.course}</h1>
+      <Part name={parts[0].name} exercises={parts[0].exercises} />
+      <Part name={parts[1].name} exercises={parts[1].exercises} />
+      <Part name={parts[2].name} exercises={parts[2].exercises} />
     </>
   );
 };
 
-const Part = (props) => {
-  return (
-    <>
-      <p>
-        {props.part} {props.exercises}
-      </p>
-    </>
-  );
-};
-
-const Content = (props) => {
-  return (
-    <>
-      <Part part={props.part1} exercises={props.exercises1} />
-      <Part part={props.part2} exercises={props.exercises2} />
-      <Part part={props.part3} exercises={props.exercises3} />
-    </>
-  );
-};
-
-const Total = (props) => {
-  return (
-    <>
-      <p>Number of the exercises {props.total}</p>
-    </>
-  );
-};
+const Total = ({ total }) => <p>Number of the exercises {total}</p>;
 
 const App = () => {
-  const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <>
-      <Header course={course} />
-      <Content
-        part1={part1}
-        exercises1={exercises1}
-        part2={part2}
-        exercises2={exercises2}
-        part3={part3}
-        exercises3={exercises3}
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total
+        total={course.parts
+          .map((part) => part.exercises)
+          .reduce((value1, value2) => value1 + value2, 0)}
       />
-      <Total total={exercises1 + exercises2 + exercises3} />
     </>
   );
 };
