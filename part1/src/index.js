683700abdbb4599b2cff4fd5f91d11b90876cd69
wrom <whunter0111@gmail.com>
2023-03-14 23:10:07 -0500

    Part 1, exercise # 1.5 rocks:rocket:

--- part1/src/index.js
+++ part1/src/index.js
@@ -22,28 +22,30 @@ const Content = ({ parts }) => {
 const Total = ({ total }) => <p>Number of the exercises {total}</p>;
 
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <>
-      <Header course={course} />
-      <Content parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
       <Total
-        total={parts
+        total={course.parts
           .map((part) => part.exercises)
           .reduce((value1, value2) => value1 + value2, 0)}
       />
