53bcd9f3b307ae561f0cc9abb61b7a90b5252f25
wrom <whunter0111@gmail.com>
2023-03-28 02:10:42 -0500

    Part 2, exercise 2.6 rocks:rocket:

---
+++ part2/phone-book/src/App.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+
+const Person = ({ person }) => <li>{person.name}</li>
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas" },
+  ]);
+  const [newName, setNewName] = useState("");
+
+  const addPerson = (event) => {
+    event.preventDefault();
+
+    const personObject = {
+      name: newName,
+    };
+
+    setPersons(persons.concat(personObject));
+    setNewName("");
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  }
+
+  return (
+    <div>
+      <h2>PhoneBook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        <ul>
+          {
+            persons.map(
+              (person, index) => <Person key={index} person={person} />
+            )
+          }
+        </ul>
+      </div>
+      <div style={{ backgroundColor: "#0fff55" }}>
+        <h2>debug</h2>
+        current writing = {newName}
+      </div>
+    </div>
+  );
+}
+
+export default App;
