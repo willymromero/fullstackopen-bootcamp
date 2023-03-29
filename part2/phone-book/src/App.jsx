f8a2b8f559d171578f0eccec6d0b40a9ce5351c7
wrom <whunter0111@gmail.com>
2023-03-29 01:33:30 -0500

    Part 2, exercise 2.9 rocks:rocket:

--- part2/phone-book/src/App.jsx
+++ part2/phone-book/src/App.jsx
@@ -4,45 +4,57 @@ const Person = ({ person }) => <li>{person.name}: {person.number}</li>
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "0994761704" }
+    { name: "Arto Hellas", number: "0994761704" },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]);
-  const [newName, setNewName] = useState("");
-  const [newNumber, setNewNumber] = useState("");
+  const [newPersonName, setNewPersonName] = useState("");
+  const [newPersonNumber, setNewPersonNumber] = useState("");
+  const [searchFilter, setSearchFilter] = useState("");
 
   const addPerson = (event) => {
     event.preventDefault();
 
-    const personExists = persons.find(person => person.name === newName);
+    const personExists = persons.find(person => person.name === newPersonName);
     if (personExists) {
-      return alert(`Whops,${newName} already exists!`);
+      return alert(`Whops,${newPersonName} already exists!`);
     }
 
     const personObject = {
-      name: newName,
-      number: newNumber,
+      name: newPersonName,
+      number: newPersonNumber,
     };
     setPersons(persons.concat(personObject));
-    setNewName("");
-    setNewNumber("");
+    setNewPersonName("");
+    setNewPersonNumber("");
   }
 
   const handleNameChange = (event) => {
-    setNewName(event.target.value);
+    setNewPersonName(event.target.value);
   }
 
   const handleNumberChange = (event) => {
-    setNewNumber(event.target.value);
+    setNewPersonNumber(event.target.value);
+  }
+
+  const handleFilterChange = (event) => {
+    setSearchFilter(event.target.value);
   }
 
   return (
     <div>
       <h2>PhoneBook</h2>
+      <div>
+        filter shown with: <input onChange={handleFilterChange} />
+      </div>
+      <h2>Add a new</h2>
       <form onSubmit={addPerson}>
         <div>
-          name: <input value={newName} onChange={handleNameChange} />
+          name: <input value={newPersonName} onChange={handleNameChange} />
         </div>
         <div>
-          number: <input value={newNumber} onChange={handleNumberChange} />
+          number: <input value={newPersonNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -52,15 +64,15 @@ const App = () => {
       <div>
         <ul>
           {
-            persons.map(
-              (person, index) => <Person key={index} person={person} />
-            )
+            persons
+              .filter(person => new RegExp(`.*${searchFilter}.*`, "i").test(person.name))
+              .map((person, index) => <Person key={index} person={person} />)
           }
         </ul>
       </div>
       <div style={{ backgroundColor: "#0fff55" }}>
         <h2>debug</h2>
-        current writing = {newName} {newNumber}
+        current writing = {newPersonName} {newPersonNumber}
       </div>
     </div>
   );
