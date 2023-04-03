279a5562f7c6f914cdade1cd061ad9ea06b2bf2a
wrom <whunter0111@gmail.com>
2023-04-02 23:18:37 -0500

    Part 2, exercise 2.11 rocks:rocket:

--- part2/phone-book/src/App.jsx
+++ part2/phone-book/src/App.jsx
@@ -1,18 +1,23 @@
+import axios from "axios";
 import Filter from "./components/filter";
 import Persons from "./components/Persons";
 import PersonForm from "./components/PersonForm";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "0994761704" },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newPersonName, setNewPersonName] = useState("");
   const [newPersonNumber, setNewPersonNumber] = useState("");
   const [searchFilter, setSearchFilter] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then(response => {
+        setPersons(response.data);
+      });
+  }, []);
+
 
   const addPerson = (event) => {
     event.preventDefault();
