cb2b29c3840abbbe98aa3c0cb3bf9f07d5f24b88
wrom <willy.maykros.romero@gmail.com>
2023-04-05 20:18:16 -0500

    Part 2, exercise 2.14 rocks:rocket:

--- part2/countries/src/components/CountryDetails.jsx
+++ part2/countries/src/components/CountryDetails.jsx
@@ -1,20 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-const CountryDetails = ({ country }) => (
-    <div>
-        <h2>{country.name.common}</h2>
+const CountryDetails = ({ country }) => {
+    const countryName = country.name.common;
+    const countryCapital = country.capital[0];
+    const countryPopulation = country.population;
+    const countryLanguages = country.languages;
+    const countryFlagSVG = country.flags.svg;
+
+    return (
         <div>
-            <div><b>Capital:</b> {country.capital[0]}</div>
-            <div><b>Population:</b> {country.population}</div>
+            <h2>{countryName}</h2>
+            <div>
+                <div><b>Capital:</b> {countryCapital}</div>
+                <div><b>Population:</b> {countryPopulation}</div>
+            </div>
+            <div>
+                <Languages languages={countryLanguages} />
+            </div>
+            <div>
+                <img src={countryFlagSVG} width={150} height={150} alt="Country Flag" ></img>
+            </div>
+            <div>
+                <Weather capital={countryCapital} />
+            </div>
         </div>
+    );
+};
+
+const Weather = ({ capital }) => {
+    const [weather, setWeather] = useState({});
+    const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
+    const apiURL = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${capital}&aqi=no`;
+
+    useEffect(() => {
+        const weatherUrlAPI = apiURL;
+        axios
+            .get(weatherUrlAPI)
+            .then(response => {
+                setWeather(response.data);
+            })
+    }, []);
+
+    if (!weather.current) {
+        return null;
+    };
+
+    return (
         <div>
-            <Languages languages={country.languages} />
+            <h2>Weather in {capital}</h2>
+            <div>
+                <p><b>Temperature °C: </b>{weather.current.temp_c}</p>
+                <p><b>Temperature °F: </b>{weather.current.temp_f}</p>
+                <img src={weather.current.condition.icon} alt={weather.current.condition.text} />
+                <p><b>Wind: </b>{weather.current.wind_kph} <b>kph</b> direction {weather.current.wind_dir}</p>
+            </div>
         </div>
-        <div>
-            <img src={country.flags.svg} width={150} height={150} alt="Country Flag" ></img>
-        </div>
-    </div>
-);
+    );
+};
 
 const Languages = ({ languages }) => {
     const languagesEntries = Object.entries(languages);
@@ -31,4 +74,4 @@ const Languages = ({ languages }) => {
     );
 };
 
-export default CountryDetails
+export default CountryDetails;
