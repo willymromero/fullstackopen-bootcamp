39c10db24a66a3707e2759b5e5e900e8010790eb
Willy Maykros Romero <94816128+maikcodes@users.noreply.github.com>
2024-02-27 21:15:49 -0500

    Part 3, exercise 3.8 rocks 🚀
    3.8*: Backend de la Agenda Telefónica, paso 8

--- part3/phone-book-api/Logger.js
+++ part3/phone-book-api/Logger.js
@@ -1,3 +1,14 @@
 import morgan from "morgan";
 
-export const logger = morgan("tiny");
+export const logger = morgan(function (tokens, req, res) {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, "content-length"),
+    "-",
+    tokens["response-time"](req, res),
+    "ms",
+    JSON.stringify(req.body),
+  ].join(" ");
+});
